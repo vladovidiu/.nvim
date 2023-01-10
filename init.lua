0d73f268c0152c38ad64fc34f9e7d1294709ea7b
vladovidiu <vlad.tutunea@shopify.com>
2023-01-10 08:55:07 +0000

    fix init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,9 @@
 -- Personal NeoVim configuration
-require "import" -- required for importing modules in a safe way
 
 require "vt.set"
 require "vt.keymap"
 require "vt.packer"
+
+require "import" -- required for importing modules in a safe way
 
 require "vt.lsp" -- lsp entry point
