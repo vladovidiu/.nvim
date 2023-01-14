f2ea8a4ee495efeaf37dc6f0d0ad94d101565f37
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2023-01-14 18:17:54 +0000

    add harpoon plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -37,4 +37,54 @@ return {
       require("mini.surround").setup(opts)
     end,
   },
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      {
+        "<leader>ha",
+        function()
+          require("harpoon.mark").add_file()
+        end,
+        desc = "Add file to harpoon",
+      },
+      {
+        "<leader>hh",
+        function()
+          require("harpoon.ui").toggle_quick_menu()
+        end,
+        desc = "Toggle harpoon menu",
+      },
+      {
+        "<leader>h1",
+        function()
+          require("harpoon.ui").nav_file(1)
+        end,
+      },
+      {
+        "<leader>h2",
+        function()
+          require("harpoon.ui").nav_file(2)
+        end,
+      },
+      {
+        "<leader>h3",
+        function()
+          require("harpoon.ui").nav_file(3)
+        end,
+      },
+      {
+        "<leader>h4",
+        function()
+          require("harpoon.ui").nav_file(4)
+        end,
+      },
+      {
+        "<leader>h5",
+        function()
+          require("harpoon.ui").nav_file(5)
+        end,
+      },
+    },
+    config = true,
+  },
 }
