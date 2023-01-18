4d982f079d9905f75eacc5e1ad89c7f5cd54ef5a
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2023-01-18 08:17:21 +0000

    more statusline to its own file

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -6,7 +6,7 @@ return {
         transparent_mode = true,
       })
 
-      vim.o.background = "dark"
+      -- vim.o.background = "dark"
       -- vim.cmd([[colorscheme gruvbox"]])
       -- vim.api.nvim_set_hl(0, "Normal", { guibg = NONE, ctermbg = NONE })
       -- vim.api.nvim_set_hl(0, "NormalFloat", { guibg = NONE, ctermbg = NONE })
