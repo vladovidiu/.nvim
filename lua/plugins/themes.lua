ea8d6c0841d602bfc912f7620ef25349d3bdadc0
Vlad Tutunea <vladovidiu.tutunea@gmail.com>
2023-01-31 19:49:33 +0000

    remove gruvbox

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,23 +1,4 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    config = function()
-      require("gruvbox").setup({
-        transparent_mode = true,
-      })
-
-      -- vim.o.background = "dark"
-      -- vim.cmd([[colorscheme gruvbox"]])
-      -- vim.api.nvim_set_hl(0, "Normal", { guibg = NONE, ctermbg = NONE })
-      -- vim.api.nvim_set_hl(0, "NormalFloat", { guibg = NONE, ctermbg = NONE })
-      -- vim.api.nvim_set_hl(0, "Pmenu", { guibg = NONE, ctermbg = NONE })
-      -- vim.api.nvim_set_hl(0, "PmenuSel", { guibg = NONE, ctermbg = NONE })
-      -- vim.api.nvim_set_hl(0, "FloatBorder", { guibg = NONE, ctermbg = NONE })
-      -- vim.api.nvim_set_hl(0, "FloatShadow", { guibg = NONE, ctermbg = NONE })
-      -- vim.api.nvim_set_hl(0, "FloatShadowThrough", { guibg = NONE, ctermbg = NONE })
-    end,
-  },
-
   -- rose-pine
   {
     "rose-pine/neovim",
