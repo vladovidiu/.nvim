e622db2b660cf632ff725d42a46a28dab90fa509
vladovidiu <vlad.tutunea@shopify.com>
2023-02-06 10:23:37 +0000

    prepare for vim-dispatch

--- lua/plugins/tasks.lua
+++ lua/plugins/tasks.lua
@@ -1,9 +1,11 @@
 return {
-  "stevearc/overseer.nvim",
-  event = "BufReadPost",
-  config = function()
-    require("overseer").setup({
-      templates = { "builtin", "vt.test_script" },
-    })
-  end,
+  {
+    "stevearc/overseer.nvim",
+    event = "BufReadPost",
+    config = function()
+      require("overseer").setup({
+        templates = { "builtin", "vt.test_script" },
+      })
+    end,
+  },
 }
