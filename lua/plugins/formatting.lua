12dd24a8105857b51ad7eaa66f85f6fe0305fc86
vladovidiu <vlad.tutunea@shopify.com>
2023-12-21 09:13:01 +0000

    update formatting

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,3 +1,8 @@
+local M = {}
+
+---@param opts ConformOpts
+function M.setup(_, opts) require("conform").setup(opts) end
+
 return {
   {
     "stevearc/conform.nvim",
@@ -18,12 +23,38 @@ return {
         callback = function(args) require("conform").format({ bufnr = args.buf }) end,
       })
     end,
-    config = function()
-      require("conform").setup({
+    ---@class ConformOpts
+    opts = function()
+      local opts = {
+        format = {
+          timeout_ms = 3000,
+          async = false,
+          quiet = false,
+        },
+
+        ---@type table<string, conform.FormatterUnit[]>
         formatters_by_ft = {
           lua = { "stylua" },
         },
-      })
+
+        formatters = {
+          injected = { options = { ignore_errors = true } },
+          -- # Example of using dprint only when a dprint.json file is present
+          -- dprint = {
+          --   condition = function(ctx)
+          --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
+          --   end,
+          -- },
+          --
+          -- # Example of using shfmt with extra args
+          -- shfmt = {
+          --   prepend_args = { "-i", "2", "-ci" },
+          -- },
+        },
+      }
+
+      return opts
     end,
+    config = M.setup,
   },
 }
