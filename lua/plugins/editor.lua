2a8c0669c51ac7791aa8523bde56da51986db024
Vlad Tutunea <vlad.tutunea@protonmail.com>
2024-02-10 15:55:23 +0000

    show hidden files in oil

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -253,6 +253,9 @@ return {
     "stevearc/oil.nvim",
     opts = {
       skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
     },
     config = function(_, opts)
       require("oil").setup(opts)
